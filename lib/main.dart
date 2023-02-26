ebac377565bdcb1d00b88c9baaecd4842e30206f
Sasaki Hiroyuki <hsasaki@tipo33.org>
2023-02-26 10:24:27 +0900

    chapter3-16-1

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static var _message = 'ok.';
-  static var _value = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -56,23 +55,30 @@ class _MyHomePageState extends State<MyHomePage> {
             const Padding(
               padding: EdgeInsets.all(10.0),
             ),
-            Slider(
-              onChanged: sliderChanged,
-              min: 0.0,
-              max: 100.0,
-              divisions: 20,
-              value: _value,
-            ),
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                    onPressed: buttonPressed,
+                    child: const Text(
+                      "tap me!",
+                      style: TextStyle(
+                          fontSize: 32.0,
+                          color: Color(0xff000000),
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ))),
           ],
         ),
       ),
     );
   }
 
-  void sliderChanged(double value) {
-    setState(() {
-      _value = value.floorToDouble();
-      _message = 'set value: $_value';
-    });
+  void buttonPressed() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) => const AlertDialog(
+              title: Text("Hello!"),
+              content: Text("This is sample."),
+            ));
   }
 }
