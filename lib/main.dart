d78a8711e0ac4c4c70ee32b5017269408d4d7839
Sasaki Hiroyuki <hsasaki@tipo33.org>
2023-02-26 09:47:56 +0900

    chater3-8-1

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static var _message = 'ok.';
-  static final _janken = <String>['グー', 'チョキ', 'パー'];
+  static final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -39,77 +39,50 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  _message,
-                  style: const TextStyle(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                _message,
+                style: const TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Roboto"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: _controller,
+                style: const TextStyle(
+                    fontSize: 28.0,
+                    color: Color(0xffFF0000),
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Roboto"),
+              ),
+            ),
+            ElevatedButton(
+                child: const Text(
+                  "Push me!",
+                  style: TextStyle(
                       fontSize: 32.0,
+                      color: Color(0xff000000),
                       fontWeight: FontWeight.w400,
                       fontFamily: "Roboto"),
                 ),
-              ),
-              TextButton(
-                  onPressed: buttonPressed,
-                  child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Text(
-                        "Push me!",
-                        style: TextStyle(
-                            fontSize: 32.0,
-                            color: Color(0xff000000),
-                            fontWeight: FontWeight.w400,
-                            fontFamily: "Roboto"),
-                      ))),
-              TextButton(
-                  onPressed: buttonPressed,
-                  child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Icon(
-                        Icons.android,
-                        size: 50.0,
-                      ))),
-              ElevatedButton(
-                  onPressed: buttonPressed,
-                  child: const Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Icon(
-                        Icons.android,
-                        size: 50.0,
-                      ))),
-              IconButton(
-                icon: const Icon(Icons.insert_emoticon),
-                iconSize: 100.0,
-                color: Colors.red,
-                onPressed: buttonPressed,
-              ),
-              FloatingActionButton(
-                  child: const Icon(Icons.android), onPressed: buttonPressed),
-              RawMaterialButton(
-                  fillColor: Colors.white,
-                  elevation: 10.0,
-                  padding: const EdgeInsets.all(10.0),
-                  child: const Text(
-                    "Push me!",
-                    style: TextStyle(
-                        fontSize: 32.0,
-                        color: Color(0xff000000),
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "Roboto"),
-                  ),
-                  onPressed: buttonPressed)
-            ]),
+                onPressed: buttonPressed),
+          ],
+        ),
       ),
     );
   }
 
   void buttonPressed() {
     setState(() {
-      _message = (_janken..shuffle()).first;
+      _message = 'you said: ${_controller.text}';
     });
   }
 }
