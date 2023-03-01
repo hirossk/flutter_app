f92bd4bcd1cfb36ee43d92000e3f9c6f150b75ba
Sasaki Hiroyuki <hsasaki@tipo33.org>
2023-03-01 20:05:53 +0900

    chapter4-10-1

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   static const List<Tab> tabs = <Tab>[
-    Tab(text: 'One'),
-    Tab(text: 'Two'),
-    Tab(text: 'Three'),
+    Tab(text: 'One', icon: Icon(Icons.star)),
+    Tab(text: 'Two', icon: Icon(Icons.info)),
+    Tab(text: 'Three', icon: Icon(Icons.home)),
   ];
 
   late TabController _tabController;
