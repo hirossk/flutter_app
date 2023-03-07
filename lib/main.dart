ca3ca4d8d0d078044583ba7666ee0ffaae8048dd
Sasaki Hiroyuki <hsasaki@tipo33.org>
2023-03-07 21:35:27 +0900

    ListView002

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, sort_child_properties_last
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, sort_child_properties_last, no_logic_in_create_state
 
 import 'package:flutter/material.dart';
 
@@ -28,50 +28,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static var _message = 'ok.';
-  static var _index = 0;
-  final List<String> items = [
-    'item001',
-    'item002',
-    'item003',
-    'item004',
-    'item005',
-    'item006',
-    'item007',
-    'item008',
-    'item009',
-    'item010',
-    'item11',
-    'item12',
-    'item13',
-    'item14',
-    'item15',
-    'item16',
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ListViewデモ'),
-        ),
-        body: ListView.builder(
-          itemCount: items.length, //要素の数　表示するデータ数の最大値（texts　listの最大値）
-          //指定した要素の数分を生成
-          itemBuilder: (context, index) {
-            return ListTile(
-              onTap: onTapped,
-              leading: Icon(Icons.person),
-              title: Text('${items[index]}'),
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListViewデモ"),
       ),
+      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
+        return Text(index.toString());
+      }),
     );
-  }
-
-  void onTapped() {
-    setState(() {});
   }
 }
