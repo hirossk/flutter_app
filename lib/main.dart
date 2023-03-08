c0bc6b0595310da5831c0c7cf258980148b98a71
Sasaki Hiroyuki <hsasaki@tipo33.org>
2023-03-08 21:28:43 +0900

    chapter6-8-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:ui' as ui;
 
 void main() => runApp(MyApp());
@@ -28,15 +29,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _controller = TextEditingController();
-  double _r = 0.0;
-  double _g = 0.0;
-  double _b = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-    loadPref();
-  }
+  static const host = 'baconipsum.com';
+  static const path = '/api/?type=meat-and-filler&paras=1&format=text';
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: <Widget>[
             const Text(
-              'PREFERENCES ACCESS.',
+              'INTERNET ACCESS.',
               style: TextStyle(fontSize: 32, fontWeight: ui.FontWeight.w500),
             ),
             const Padding(padding: EdgeInsets.all(10.0)),
@@ -61,79 +55,29 @@ class _MyHomePageState extends State<MyHomePage> {
               minLines: 1,
               maxLines: 5,
             ),
-            const Padding(padding: EdgeInsets.all(10.0)),
-            Slider(
-              min: 0.0,
-              max: 255.0,
-              value: _r,
-              divisions: 255,
-              onChanged: (double value) {
-                setState(() {
-                  _r = value;
-                });
-              },
-            ),
-            Slider(
-              min: 0.0,
-              max: 255.0,
-              value: _g,
-              divisions: 255,
-              onChanged: (double value) {
-                setState(() {
-                  _g = value;
-                });
-              },
-            ),
-            Slider(
-              min: 0.0,
-              max: 255.0,
-              value: _b,
-              divisions: 255,
-              onChanged: (double value) {
-                setState(() {
-                  _b = value;
-                });
-              },
-            ),
-            Container(
-              padding: const EdgeInsets.all(20),
-              width: 125,
-              height: 125,
-              color: Color.fromARGB(255, _r.toInt(), _g.toInt(), _b.toInt()),
-            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.open_in_new),
         onPressed: () {
-          savePref();
+          getData();
           showDialog(
               context: context,
               builder: (BuildContext context) => const AlertDialog(
-                    title: Text("saved!"),
-                    content: Text("save preferences."),
+                    title: Text("loaded!"),
+                    content: Text("get content from URI."),
                   ));
         },
       ),
     );
   }
 
-  void loadPref() async {
-    final prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _r = (prefs.getDouble('r') ?? 0.0);
-      _g = (prefs.getDouble('g') ?? 0.0);
-      _b = (prefs.getDouble('b') ?? 0.0);
-      _controller.text = (prefs.getString('input') ?? '');
-    });
-  }
-
-  void savePref() async {
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setDouble('r', _r);
-    prefs.setDouble('g', _g);
-    prefs.setDouble('b', _b);
-    prefs.setString('input', _controller.text);
+  void getData() async {
+    var http = await HttpClient();
+    HttpClientRequest request = await http.get(host, 80, path);
+    HttpClientResponse response = await request.close();
+    final value = await response.transform(utf8.decoder).join();
+    _controller.text = value;
   }
 }
